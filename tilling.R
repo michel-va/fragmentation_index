a6e792313130a14901ebdd6d3774a5ae185e9c24
Michel Valette <m.valette20@imperial.ac.uk>
2022-02-11 12:51:49 +0000

    correction

--- tilling.R
+++ tilling.R
@@ -8,7 +8,3 @@ splitRaster(lu,3,3,buffer=0.05,fExt=".tif",path="tilled_raster")
 
 # The second and third argument indicate how many time you want to divide the 
 # raster horizontally and vertically 
-
-
-
-### try 
